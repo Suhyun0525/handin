f1db442736a2928a7bfff2f36cca43c35df8ef51
suhyun0525 <blakestairs1@gmail.com>
2021-09-23 11:17:51 -0400

    hw5

--- hw5_shwang/acc.py
+++ hw5_shwang/acc.py
@@ -1,4 +1,4 @@
-def acceleration(u1, u2, t1, t2):
+def acceleration( u1, u2, t1, t2 ):
     """Function hat takes the different speeds (u1,u2) of a body at times t1 and t2 and 
     calculates the acceleration of the body"""
 
--- hw5_shwang/displacement.py
+++ hw5_shwang/displacement.py
@@ -1,7 +1,10 @@
 def displacement(u_init, t, a):
     """Function that calculates the total displacement of a body during a time intervalt, 
     when the body has initial speedu_init and a constant accelerationa"""
+    # t = total timethe object moves
+    # a = the constant acceleration
+    # u_init = the initial speed of the body
     
     s = (u_init) * t + 0.5 * a * t**2
-    
+    return s
     
