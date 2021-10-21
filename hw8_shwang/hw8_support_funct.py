55e845a9586f9bdbc816006d8724921c5b5e1c5e
suhyun0525 <blakestairs1@gmail.com>
2021-10-21 01:09:59 -0400

    hw8

--- hw8_shwang/hw8_support_funct.py
+++ hw8_shwang/hw8_support_funct.py
@@ -1,7 +1,7 @@
 def order_array(input_array):
     """function that rearranges the numbers in an array in smaller to larger order.\
-    Input: An array of number 
-    Output: The array of nunber rearranged in order """
+    Input: An array of number (numbers)
+    Output: The array of number rearranged in order (numbers) """
     
     for i in range(len(input_array)):
         m_index = i
